a642dc69ff1d4f1c275169746f2b246a71ca1850
Kolawole Joshua <Kolawole.joshua@wemabank.com>
2023-11-03 08:33:15 +0100

    finalized job details page

--- src/view/JobSeeker/Dashboard/JobDetails.jsx
+++ src/view/JobSeeker/Dashboard/JobDetails.jsx
@@ -7,6 +7,11 @@ import { CustomButton } from "../../../components/buttons/CustomButton";
 import { ReactComponent as CompanyIcon } from "../../../assets/svg/company-icon.svg";
 import { ReactComponent as Bookmark } from "../../../assets/svg/fluent_bookmark-32-regular.svg";
 import { ReactComponent as Map } from "../../../assets/svg/MapTrifold.svg";
+import { ReactComponent as Calendar } from "../../../assets/svg/CalendarBlank.svg";
+import { ReactComponent as Timer } from "../../../assets/svg/Timer.svg";
+import { ReactComponent as Stack } from "../../../assets/svg/Stack.svg";
+import { ReactComponent as Wallet } from "../../../assets/svg/Wallet.svg";
+import { ReactComponent as Brief } from "../../../assets/svg/briefcase.svg";
 
 const JobDetails = () => {
   const userData = useSelector((state) => state?.auth?.data);
@@ -169,7 +174,7 @@ const JobDetails = () => {
               </div>
 
               <div className="flex justify-end items-center mt-10">
-                <div className="p-5 border-2 rounded-lg w-8/12 bg-white border-[#E7F0FA] flex items-center gap-10 justify-between">
+                <div className="p-5 border-2 rounded-lg w-10/12 bg-white border-[#E7F0FA] flex items-center gap-10 justify-between">
                   <div className="text-center space-y-3 ">
                     <div className="text-lg font-semibold">Salary (Naira)</div>
                     <div className="text-xl font-semibold">
@@ -177,7 +182,7 @@ const JobDetails = () => {
                     </div>
                     <div className="text-lg">Yearly salary</div>
                   </div>
-                  <div className="text-center">
+                  <div className="text-center space-y-3">
                     <div className="flex justify-center">
                       <Map />
                     </div>
@@ -186,6 +191,90 @@ const JobDetails = () => {
                   </div>
                 </div>
               </div>
+
+              <div className="mt-10">
+                <div className="flex justify-end items-center">
+                  <div className="p-5 border-2 rounded-lg w-10/12 bg-white border-[#E7F0FA]">
+                    <div className="my-5 font-semibold text-lg">
+                      Job Overview
+                    </div>
+                    <div className="grid grid-cols-3 gap-10">
+                      <div className="text-center space-y-2 ">
+                        <Calendar />
+                        <div className="text-md font-medium text-left">
+                          Job Posted
+                        </div>
+                        <div className="text-lg text-left">14 Jun, 2023</div>
+                      </div>
+
+                      <div className="text-center space-y-2 ">
+                        <Timer />
+                        <div className="text-md font-medium text-left">
+                          Job Expire In
+                        </div>
+                        <div className="text-lg text-left">14 Aug, 2023</div>
+                      </div>
+
+                      <div className="text-center space-y-2 ">
+                        <Stack />
+                        <div className="text-md font-medium text-left">
+                          Job Level
+                        </div>
+                        <div className="text-lg text-left">Senior Level</div>
+                      </div>
+
+                      <div className="text-center space-y-2 ">
+                        <Wallet />
+                        <div className="text-md font-medium text-left">
+                          Experience
+                        </div>
+                        <div className="text-lg text-left">3-5 years</div>
+                      </div>
+
+                      <div className="text-center space-y-2 ">
+                        <Brief />
+                        <div className="text-md font-medium text-left">
+                          Education
+                        </div>
+                        <div className="text-lg text-left">Certification</div>
+                      </div>
+                    </div>
+
+                    <div className="mt-10">
+                      <hr />
+                      <div className="my-4">
+                        <p className="font-semibold text-lg">Share this Job:</p>
+                      </div>
+                      <div className="flex gap-1">
+                        <div className="text-[#0A65CC] text-xs rounded-lg px-2 py-2 bg-[#E7F0FA]">
+                          🔗Copy Links
+                        </div>
+                        <div className="text-[#0A65CC] text-xs rounded-lg px-2 py-2 bg-[#E7F0FA]">
+                          Linkedin
+                        </div>
+                        <div className="text-[#0A65CC] text-xs rounded-lg px-2 py-2 bg-[#E7F0FA]">
+                          Facebook
+                        </div>
+                        <div className="text-[#0A65CC] text-xs rounded-lg px-2 py-2 bg-[#E7F0FA]">
+                          Twitter
+                        </div>
+                        <div className="text-[#0A65CC] text-xs rounded-lg px-2 py-2 bg-[#E7F0FA]">
+                          Mail
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-20 mb-5 space-y-3">
+            <hr />
+
+            <div className="text-center text-sm">
+              2023 © All rights reserved |InCorp Hr Web App. | Designed and
+              Developed by the InCorp Team.
             </div>
           </div>
         </div>
